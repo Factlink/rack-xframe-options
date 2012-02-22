0a25ed204b10805d2f1835f5529d8dd5663d91f6
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2012-02-22 17:03:48 +0100

    Removed old test suite

--- test/helper.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require "rack/test"
-require 'shoulda'
-
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'rack/xframe-options'
